3b824c2a3baa7fb8203cf830dd40bf7fc58a6b7b
Vova <wishmaster2310@gmail.com>
2016-07-13 12:46:19 +0500

    up

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,17 @@
 $(function() {
 
 
-	$('.c-hero__slider').slick({
+	$('.c-slider').slick({
 		arrows: false,
 		autoplay: true
 	});
 
-	$('.c-appForm__input--tel').mask('+7 (999) 999-99-99')
+	$('.c-appForm__input--tel').mask('+7 (999) 999-99-99');
+	$('.c-nav__item--list').on('click', function(e) {
+		e.preventDefault();
+		if ((e.target).tagName === 'A') { return }
+
+		$($(this).find('.c-subNav')).stop(true, true).slideToggle()
+	})
 
 });
