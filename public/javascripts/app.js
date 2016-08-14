a3f2ed5c80a66f24a9ecb6847f627be7b8717a88
wishmaster <wishmaster2310@gmail.com>
2016-08-14 15:50:20 +0500

    up

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,7 +4,15 @@ $(function() {
 	$('.c-slider').slick({
 		autoplay: true,
 		prevArrow: "<div class='c-slider__arrow c-slider__arrow--prev'>",
-		nextArrow: "<div class='c-slider__arrow c-slider__arrow--next'>"
+		nextArrow: "<div class='c-slider__arrow c-slider__arrow--next'>",
+		responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        arrows: false,
+        dots: true
+      }
+    }]
 	});
 
 	$('.c-appForm__input--tel').mask('+7 (999) 999-99-99');
@@ -14,7 +22,23 @@ $(function() {
 			e.preventDefault();
 		$(this).toggleClass('c-nav__item--active');
 		$($(this).find('.c-subNav')).stop(true, true).slideToggle();
+	});
 
+	$('.c-header__burger').on('click', function() {
+		if (!$(this).hasClass('c-header__burger--active')) {
+			$(this).addClass('c-header__burger--active');
+			$('.c-navBox').slideDown();
+		} else {
+			$(this).removeClass('c-header__burger--active')
+			$('.c-navBox').slideUp();
+		}
+	});
+
+	$(window).on('resize', function() {
+
+		$('.c-navBox').stop().attr('style', '');
+		$('.c-header__burger').removeClass('c-header__burger--active')
+		
 	})
 
 });
